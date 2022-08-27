092641ce41514eecfc341cc926a7b3e7a6cc41ff
andygeiss <4ndygeiss@gmail.com>
2022-08-27 12:15:38 +0200

    builder pattern added

--- internal/app/generate.go
+++ internal/app/generate.go
@@ -11,17 +11,19 @@ import (
 func Generate(cfg *Config) []*entity.Entity {
 	out := make([]*entity.Entity, cfg.NumberOfEntities)
 	for i := range out {
-		out[i] = entity.NewEntity(fmt.Sprintf("%d", i), []entity.Component{
-			components.NewPosition().(*components.Position).
-				WithX(rand.Float32() * float32(cfg.Width)).
-				WithY(rand.Float32() * float32(cfg.Height)),
-			components.NewSize().(*components.Size).
-				WithWidth(3).
-				WithHeight(3),
-			components.NewVelocity().(*components.Velocity).
-				WithX(rand.Float32() * 10).
-				WithY(rand.Float32() * 10),
-		})
+		out[i] = entity.NewEntity().
+			WithID(fmt.Sprintf("%d", i)).
+			WithComponents(
+				components.NewPosition().(*components.Position).
+					WithX(rand.Float32()*float32(cfg.Width)).
+					WithY(rand.Float32()*float32(cfg.Height)),
+				components.NewSize().(*components.Size).
+					WithWidth(3).
+					WithHeight(3),
+				components.NewVelocity().(*components.Velocity).
+					WithX(rand.Float32()*10).
+					WithY(rand.Float32()*10),
+			)
 	}
 	return out
 }
